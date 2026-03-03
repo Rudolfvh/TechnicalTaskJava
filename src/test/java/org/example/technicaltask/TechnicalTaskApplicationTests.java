54daea7a10e20da9711f9924b14950ebc9515f35
Rudolfvh <matyane97@gmail.com>
2026-03-03 16:34:15 +0300

    ТЗ

---
+++ src/test/java/org/example/technicaltask/TechnicalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.technicaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
